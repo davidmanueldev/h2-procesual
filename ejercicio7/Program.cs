91a0c82f82da20f3f1413f74d84904debae7c991
David Manuel <davidmanuel.dev@gmail.com>
2023-09-04 10:58:50 -0400

    Primer version de los ejercicios

---
+++ ejercicio7/Program.cs
@@ -0,0 +1,19 @@
+﻿// Escribir un programa que imprima la suma de todos los números que van del 1 al 100.
+
+using System;
+
+namespace ejercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumatoria = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                sumatoria += i;
+            }
+            Console.WriteLine("La sumatoria es: " + sumatoria);
+        }
+    }
+}
---
+++ ejercicio8/Program.cs
@@ -0,0 +1,23 @@
+﻿// Introducir n números hasta que se ingrese el 9 y mostrar su sumatoria
+
+using System;
+
+namespace ejercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, sumatoria = 0;
+            Console.WriteLine("Ingrese un numero: ");
+            n = int.Parse(Console.ReadLine());
+            while (n != 9)
+            {
+                sumatoria += n;
+                Console.WriteLine("Ingrese un numero: ");
+                n = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("La sumatoria es: " + sumatoria);
+        }
+    }
+}
